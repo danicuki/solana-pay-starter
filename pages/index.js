fc726decf78ea8fdd4fccc27a597feb80ca0e423
Daniel Cukier <danicuki@gmail.com>
2022-07-27 14:36:53 -0300

    finished solving

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,36 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React from "react"
+import { PublicKey } from "@solana/web3.js"
+import { useWallet } from "@solana/wallet-adapter-react"
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = "web3dev_"
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
-  
-  
+  const { publicKey } = useWallet()
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  )
+
   return (
     <div className="App">
-      <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
+          <p className="header"> 😳 WEB3DEV Emoji Store 😈</p>
           <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? "Connected!" : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
@@ -32,7 +44,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
